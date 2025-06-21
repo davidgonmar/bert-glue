0fa98815d3d3307ba4ddb59785b40d96c1cc2770
David González Martínez <me@davidgonmar.me>
2025-06-21 13:53:06 +0200

    fix train_all.sh

--- train_all.sh
+++ train_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 TASKS=(cola sst2 mrpc qqp stsb mnli qnli rte wnli)
 for TASK in "${TASKS[@]}"; do
-  python train_glue.py --task_name "$TASK" --output_dir "./outputs/${TASK}"
+  python train.py --task_name "$TASK" --output_dir "./outputs/${TASK}"
 done
